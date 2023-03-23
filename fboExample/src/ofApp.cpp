fb47bb3a412ee550e63ec94a5b7d610dc858b5be
Miguel Espada <miguel.valero.espada@gmail.com>
2023-03-23 17:35:37 +0100

    fbo

--- fboExample/src/ofApp.cpp
+++ fboExample/src/ofApp.cpp
@@ -2,13 +2,10 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-    
-    material.setDiffuseColor(ofColor::red);
-    material2.setDiffuseColor(ofColor::gray);
+    material.setDiffuseColor(ofColor::white);
 
     light.setPosition(200, 200, 200);
     light.setDiffuseColor(ofColor::white);
-
     plane.set(2000, 2000);
     plane.rotateDeg(-90, 1, 0, 0);
     plane.move(0, -50.1, 0);
@@ -17,130 +14,63 @@ void ofApp::setup(){
 
     cam.setPosition(0, 500, -1000);
     cam.setNearClip(100);
-
     cam.setFov(60);
 
-
-
-    speed = 0;
-
-    //faro1.enable();
-    faro1.setParent(box);
-    faro1.setDiffuseColor(ofColor::yellow);
-    faro1.setSpotlight();
-    faro1.move(0, 0, 50);
-    faro1.rotateDeg(200, 1, 0, 0);
-
-
-    ceilCam.setPosition(0, 2000, 0);
-    ceilCam.rotateDeg(-90, 1, 0, 0);
-    ceilCam.rotateDeg(180, 0, 1, 0);
-    ceilCam.disableMouseInput();
-
+    
     ofEnableNormalizedTexCoords();
     ofDisableArbTex();
-    img.load("cat.png");
-
-    texture = img.getTexture();
-    texture.setTextureWrap( GL_REPEAT, GL_REPEAT );
-    texture.generateMipmap();
-    texture.setTextureMinMagFilter(GL_LINEAR_MIPMAP_LINEAR, GL_NEAREST);
-
-
-    fbo.allocate(800, 800);
+    
+    fbo.allocate(400, 400);
+    
+    
+ 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
     
-    cam.setTarget(box);
-    box.move(box.getZAxis() * speed);
-    
-    
-    
-    if(ofGetKeyPressed(OF_KEY_LEFT))
-        box.rotateDeg(1, 0, 2, 0);
-    if(ofGetKeyPressed(OF_KEY_RIGHT))
-        box.rotateDeg(-1, 0, 2, 0);
-    
-    if(ofGetKeyPressed(OF_KEY_UP)){
-        speed += 0.1;
-    }
-    
-    if(ofGetKeyPressed(OF_KEY_DOWN)){
-        speed -= 0.1;
-    }
-    
+    cam.setTarget(plane);
+   
     fbo.begin();
     {
-       ofBackground(255);
-       ceilCam.begin();
-        {
-            drawScene();
+        ofBackground(255);
+        for(int i = 0; i < 100; i ++){
+            ofSetColor(ofRandom(255), ofRandom(255), ofRandom(255));
+            ofDrawCircle(ofRandom(400), ofRandom(400),10);
         }
-        ceilCam.end();
     }
     fbo.end();
     
-}
-
-void ofApp::drawScene(){
+ 
     
-    
-    ofEnableDepthTest();
-        light.draw();
-        faro1.draw();
-        material.begin();
-        {
-            box.draw();
-            box.drawAxes(100);
-        }
-        material.end();
-        material2.begin();
-        {
-           // fbo.getTexture().bind();
-            
-                plane.mapTexCoords(0, 0, 1, 1);
-                plane.draw();
-            
-             //   fbo.getTexture().unbind();
-        }
-        material2.end();
-//        ofSetColor(0, 255, 0);
-//        plane.drawNormals(100);
-
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-    ofBackground(ofColor::black);
-
+    ofBackground(0, 0, 0);
     ofEnableLighting();
+
     light.enable();
-    faro1.enable();
     
-    ofEnableDepthTest();
+    
     cam.begin();
     {
-        drawScene();
+        material.begin();
+        {
+            fbo.getTexture().bind();
+            {
+                plane.draw();
+            }
+            fbo.getTexture().unbind();
+        }
+        material.end();
     }
     cam.end();
-    
-    
-    ofDisableDepthTest();
-    fbo.draw(mouseX, mouseY, 200, 200);
-
-
-
-
-
-    
-
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-  
+
 }
 
 //--------------------------------------------------------------
